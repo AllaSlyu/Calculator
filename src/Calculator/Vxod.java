4eee49ef80015630417108125c2be2341855f9b0
AllaSlyusarenko <3696588@gmail.com>
2022-08-27 16:24:57 +0300

    добавила in.close()

--- src/Calculator/Vxod.java
+++ src/Calculator/Vxod.java
@@ -7,12 +7,13 @@ public  class Vxod {
         Scanner in = new Scanner(System.in);//для получения ввода с консоли в классе System определен объект in. Класс Scanner использует System.in
         System.out.print("vvedite primer:");
         String input = in.nextLine();//считывает всю строку и возвращает её
-        String result = null;
+        String result;
         try {
             result = Main.calc(input);
         } catch (AllExceptions allExceptions) {
             result = allExceptions.getMessage();
         }
         System.out.println(result);
+        in.close();
     }
 }
